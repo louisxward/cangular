8132b8a74f310bde1671f1fc7874ae6039962370
lward <louis.ward@ocgsoftware.com>
2023-01-22 19:10:36 +0000

    login

--- src/app/Login/components/login-form/login-form.component.ts
+++ src/app/Login/components/login-form/login-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router'
+import { Store } from "@ngxs/store";
+import { User, UserState } from "src/app/Core/state/user";
 
 @Component({
   selector: 'app-login-form',
@@ -13,19 +15,12 @@ export class LoginFormComponent implements OnInit{
 
     loginForm:FormGroup;
 
-    favoriteColorControl = new FormControl('');
-
-    constructor(private router: Router, private fb:FormBuilder){
+    constructor(private router: Router, private fb:FormBuilder, private store: Store){
         this.loginForm = this.fb.group({
             username: ["", [Validators.required]],
             password: ["", [Validators.required]]
         });
-
     }
-
-    get name() { return this.loginForm.get('username'); }
-
-    get power() { return this.loginForm.get('password'); }
 
     ngOnInit(): void {
     }
@@ -33,5 +28,18 @@ export class LoginFormComponent implements OnInit{
     submit(): void {
         console.log("Form Submitted")
         console.log(this.loginForm.value)
+        this.login()
     }
+
+
+    login(): void {
+        this.store.dispatch(
+          new User.Login.LoginFlowInitiated({
+            username: this.loginForm.value.username,
+            password: this.loginForm.value.password
+          })
+        );
+      }
+
+
 }
