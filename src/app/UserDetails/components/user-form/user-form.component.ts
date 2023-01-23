4989450f6649c52e7d7542a64676acbc7eb457c7
lward <louis.ward@ocgsoftware.com>
2023-01-23 17:46:21 +0000

    can create and save!

--- src/app/UserDetails/components/user-form/user-form.component.ts
+++ src/app/UserDetails/components/user-form/user-form.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { Router, ActivatedRoute, ParamMap } from '@angular/router'
 import { Action, StateContext, Store } from "@ngxs/store";
 import { User, UserState, UserStateModel } from "src/app/Core/state/user";
+import PocketBase from 'pocketbase'
 
 
 @Component({
@@ -13,7 +14,9 @@ import { User, UserState, UserStateModel } from "src/app/Core/state/user";
 
 
 export class UserFormComponent implements OnInit{
-    @Input('userData') userData = {
+  pb = new PocketBase('http://127.0.0.1:8090')  
+  
+  @Input('userData') userData = {
       id: "",
       username: "",
       email: "",
@@ -21,19 +24,51 @@ export class UserFormComponent implements OnInit{
 
     userForm:FormGroup
 
-    constructor(private fb:FormBuilder){
+    constructor(private router: Router, private fb:FormBuilder){
       this.userForm = this.fb.group({})
     }
 
     ngOnInit(): void {
       this.userForm.addControl( 'username', new FormControl(this.userData.username, Validators.required))
-      this.userForm.addControl( 'email', new FormControl(this.userData.email, Validators.required))
-      console.log(this.userData.id)
+      this.userForm.addControl( 'email', new FormControl(this.userData.email, [Validators.required, Validators.email]))//only admins can edit emails, creation still allows anyone to set the email
+      if(this.userData.id == "0"){
+        this.userForm.addControl( 'emailVisibility', new FormControl(true, Validators.required))
+        this.userForm.addControl( 'password', new FormControl("", Validators.required))
+        this.userForm.addControl( 'passwordConfirm', new FormControl("", Validators.required))
+      }
     }
 
     submit(): void {
         console.log("Form Submitted")
         console.log(this.userForm.value)
+        if(this.userData.id == "0"){
+          this.createUser()
+        }
+        else{
+          this.saveUser()
+        }
+    }
+
+    async saveUser(){
+      const myPromise = this.pb.collection('users').update(this.userData.id, this.userForm.value);
+      await myPromise.then((value) => { 
+        console.log("user saved")
+        this.router.navigate(["users"]);//see below comment, added this so it matches current functionality
+      })
+      .catch((error)=>{ 
+        console.log(error)
+      }) 
+    }
+
+    async createUser(){
+      const myPromise = this.pb.collection('users').create(this.userForm.value);
+      await myPromise.then((value) => { 
+        console.log("user created")
+        this.router.navigate(["users"]);//should refresh page and show as save instead of redirecting to user list
+      })
+      .catch((error)=>{ 
+        console.log(error)
+      })
     }
 
 }
