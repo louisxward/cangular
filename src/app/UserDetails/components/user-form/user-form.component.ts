8f6b7140c3ed1d34dc092833df61a0567527296f
lward <louis.ward@ocgsoftware.com>
2023-01-23 16:46:54 +0000

    new formmmmmm

--- src/app/UserDetails/components/user-form/user-form.component.ts
+++ src/app/UserDetails/components/user-form/user-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { Router, ActivatedRoute, ParamMap } from '@angular/router'
 import { Action, StateContext, Store } from "@ngxs/store";
@@ -11,18 +11,24 @@ import { User, UserState, UserStateModel } from "src/app/Core/state/user";
   styleUrls: ['./user-form.component.scss']
 })
 
-export class UserFormComponent implements OnInit{
 
+export class UserFormComponent implements OnInit{
+    @Input('userData') userData = {
+      id: "",
+      username: "",
+      email: "",
+    } 
 
     userForm:FormGroup
 
     constructor(private fb:FormBuilder){
-      this.userForm = this.fb.group({
-        username: ["", [Validators.required]],
-    });
+      this.userForm = this.fb.group({})
     }
 
     ngOnInit(): void {
+      this.userForm.addControl( 'username', new FormControl(this.userData.username, Validators.required))
+      this.userForm.addControl( 'email', new FormControl(this.userData.email, Validators.required))
+      console.log(this.userData.id)
     }
 
     submit(): void {
--- src/app/UserDetails/userDetails/userDetails.component.ts
+++ src/app/UserDetails/userDetails/userDetails.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PageHeaderComponent } from '../../Core/components/page-header/page-header.component';
 import { UserFormComponent } from '../components/user-form/user-form.component'
+import PocketBase from 'pocketbase'
 
 
 
@@ -12,10 +13,48 @@ import { UserFormComponent } from '../components/user-form/user-form.component'
 })
 export class UserDetailsComponent {
 
-  userId: string | null
+  pb = new PocketBase('http://127.0.0.1:8090')
 
+  userId: string
+  loaded = false
+  found = false
+  userData = {
+    id: "0",
+    username: "",
+    email: "",
+  }
+  
   constructor(private route: ActivatedRoute) {
-    this.userId = this.route.snapshot.paramMap.get("userId")
+    const param = this.route.snapshot.paramMap.get("userId")
+    this.userId = param ? param : "0"
+    if(this.userId != "0"){
+      this.loadUser()
+    }
+    else{
+      this.loaded = true
+      this.found = true
+    }
   }
 
+  ngOnInit() {
+  }
+
+  async loadUser(){
+    console.log("loadUser()")
+    const myPromise = this.pb.collection('users').getOne(this.userId, {})
+    await myPromise.then((value) => { 
+      console.log("User Found")
+      this.userData = {
+        id: value.id,
+        username: value.username,
+        email: value.email,
+      }
+      this.found = true
+    })
+   .catch((error)=>{ 
+      console.log(error)
+      console.log("User Not Found")
+    })
+    this.loaded = true
+  }
 }
