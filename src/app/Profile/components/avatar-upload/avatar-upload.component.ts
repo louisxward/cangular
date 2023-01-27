89c113cdd4cd2ec777281c2dc962b1d66a233728
louisxward <louisxward@gmail.com>
2023-01-27 12:18:33 +0000

    add upload

--- src/app/Profile/components/avatar-upload/avatar-upload.component.ts
+++ src/app/Profile/components/avatar-upload/avatar-upload.component.ts
@@ -10,6 +10,16 @@ import { UploadService } from 'src/app/Core/services/upload/upload.service';
 
 export class AvatarUploadComponent implements OnInit{
 
+
+  formData = new FormData();
+
+  fileToUpload: File | null = null;
+  
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+    this.formData.append('fileKey', this.fileToUpload, this.fileToUpload?.name);//<<<!<!<!<>!>!>!>!>!>>>!>!>!!>?!?!??!
+  }
+
   constructor(private uploadService : UploadService){
   }
   
