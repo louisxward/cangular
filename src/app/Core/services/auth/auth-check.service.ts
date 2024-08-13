ea0b1dd3359190a7302888f313e26393a9b096c3
Louis Ward <louis.ward@ocgsoftware.com>
2024-08-13 20:51:42 +0100

    49 - add enable disable for inactivty check

--- src/app/Core/services/auth/auth-check.service.ts
+++ src/app/Core/services/auth/auth-check.service.ts
@@ -14,56 +14,42 @@ export class AuthCheckService {
 		private apiService: ApiService,
 		private loginService: LoginService
 	) {
-		console.log('AuthCheckService()') // ToDo - Remove when confident
-		this.loginService.onLoginChange().subscribe((e) => this.switchTimer(e)) //ToDo - What does this pass into switch timer?
+		this.loginService.onLoginChange().subscribe((e) => this.switchTimer(e))
 	}
-	private timeoutDuration = 1 * 60 * 1000 // ToDo - Not sure on this time at the moment
+	private timeoutDuration = 1 * 60 * 1000
 	private timeout: any
 
 	private enableTimer() {
-		console.log('AuthCheckService().enableTimer()') // ToDo - Remove when confident
 		if (!this.timeout) {
 			this.startTimer()
 		}
 	}
 
 	private switchTimer(enable: boolean) {
-		console.log('AuthCheckService().switchTimer()') // ToDo - Remove when confident
 		enable ? this.enableTimer() : this.disableTimer()
 	}
 
 	private disableTimer() {
-		console.log('AuthCheckService().disableTimer()') // ToDo - Remove when confident
 		if (this.timeout) {
+			clearTimeout(this.timeout)
 			this.timeout = null
 		}
 	}
 
 	private startTimer() {
-		console.log('AuthCheckService().startTimer()') // ToDo - Remove when confident
 		this.timeout = setTimeout(async () => {
-			console.log('AuthCheckService().startTimer().this.timeout: ' + this.timeout) // ToDo - Remove when confident
-			// Incase user logs out whilst timer is still active
-			if (this.timeout) {
-				await this.loginService.checkAuth().then((e) => {
-					if (!e) {
-						this.logout()
-					} else {
-						this.resetTimer()
-					}
-				})
-			}
+			await this.loginService.checkAuth().then((e) => {
+				if (!e) {
+					this.loginService.logout(true)
+				} else {
+					this.resetTimer()
+				}
+			})
 		}, this.timeoutDuration)
 	}
 
 	private resetTimer() {
-		console.log('AuthCheckService().resetTimer()') // ToDo - Remove when confident
 		clearTimeout(this.timeout)
 		this.startTimer()
-	}
-
-	private logout() {
-		console.log('AuthCheckService().logout()') // ToDo - Remove when confident
-		this.loginService.logout(true)
 	}
 }
--- src/app/Core/services/auth/auth-inactivity.service.ts
+++ src/app/Core/services/auth/auth-inactivity.service.ts
@@ -6,32 +6,64 @@ import { LoginService } from '../login/login.service'
 })
 export class AuthInactivityService {
 	constructor(private loginService: LoginService) {
-		this.startTimer()
-		this.setupActivityListeners()
+		this.loginService.onLoginChange().subscribe((e) => this.switchTimer(e)) //ToDo - What does this pass into switch timer?
 	}
-	private timeoutDuration = 5 * 60 * 1000 // 5 minutes in milliseconds
+	private timeoutDuration = 0.1 * 60 * 1000 // 5 minutes in milliseconds
 	private timeout: any
 
+	private enableTimer() {
+		console.log('enableTimer')
+		if (!this.timeout) {
+			this.startTimer()
+			this.enableActivityListeners()
+		}
+	}
+
+	private switchTimer(enable: boolean) {
+		console.log('switchTimer')
+		enable ? this.enableTimer() : this.disableTimer()
+	}
+
+	private disableTimer() {
+		console.log('disableTimer')
+		if (this.timeout) {
+			clearTimeout(this.timeout)
+			this.timeout = null
+			this.disableActivityListeners()
+		}
+	}
+
 	private startTimer() {
+		console.log('startTimer')
 		this.timeout = setTimeout(() => {
 			this.logout()
 		}, this.timeoutDuration)
 	}
 
 	private resetTimer() {
-		clearTimeout(this.timeout)
-		this.startTimer()
+		console.log('resetTimer')
+		if (this.timeout) {
+			clearTimeout(this.timeout)
+			this.startTimer()
+		}
 	}
 
 	private logout() {
+		console.log('logout')
 		this.loginService.logout(true)
 	}
 
-	private setupActivityListeners() {
-		window.addEventListener('mousemove', () => this.resetTimer())
+	private enableActivityListeners() {
 		window.addEventListener('keydown', () => this.resetTimer())
 		window.addEventListener('click', () => this.resetTimer())
 		window.addEventListener('scroll', () => this.resetTimer())
 		window.addEventListener('touchstart', () => this.resetTimer())
+	}
+
+	private disableActivityListeners() {
+		window.removeEventListener('keydown', () => this.resetTimer())
+		window.removeEventListener('click', () => this.resetTimer())
+		window.removeEventListener('scroll', () => this.resetTimer())
+		window.removeEventListener('touchstart', () => this.resetTimer())
 	}
 }
