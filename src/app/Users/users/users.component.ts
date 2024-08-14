b5c0ba04a3de73a65ef1c52e0880969efa3950d1
Louis Ward <louis.ward@ocgsoftware.com>
2024-08-14 23:05:14 +0100

    74 - more types

--- src/app/Users/users/users.component.ts
+++ src/app/Users/users/users.component.ts
@@ -12,6 +12,11 @@ interface DataType {
 	email: string
 }
 
+interface Action {
+	label: string
+	action: (item: DataType) => void
+}
+
 @Component({
 	selector: 'app-users',
 	templateUrl: './users.component.html',
@@ -24,22 +29,22 @@ export class UsersComponent {
 		{ header: 'Email', field: 'email' },
 	]
 
-	data = [
+	data: DataType[] = [
 		{ name: 'John Doe', age: 25, email: 'john.doe@example.com' },
 		{ name: 'Jane Smith', age: 30, email: 'jane.smith@example.com' },
 		{ name: 'Mike Johnson', age: 35, email: 'mike.johnson@example.com' },
 	]
 
-	actions = [
-		{ label: 'Edit', action: (item: any) => this.editItem(item) },
-		{ label: 'Delete', action: (item: any) => this.deleteItem(item) },
+	actions: Action[] = [
+		{ label: 'Edit', action: (item: DataType) => this.editItem(item) },
+		{ label: 'Delete', action: (item: DataType) => this.deleteItem(item) },
 	]
 
-	editItem(item: any) {
+	editItem(item: DataType) {
 		console.log('Edit', item)
 	}
 
-	deleteItem(item: any) {
+	deleteItem(item: DataType) {
 		console.log('Delete', item)
 	}
 }
